bb1cd6878bde918f9e7f2f2d4742e7d427e0c4a9
rubencnik <rubehbardakhchyan@gmail.com>
2025-09-09 13:15:28 +0300

    Add files via upload

---
+++ java.java
@@ -0,0 +1,15 @@
+#Создание массива данных типа "список" на Java
+
+ArrayList<String> fruits = new ArrayList<String>();
+fruits.add("apple");
+fruits.add("banana");
+fruits.add("watermelon");
+
+#Организация стека на Java
+
+Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
