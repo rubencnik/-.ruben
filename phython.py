12e5bd7f8aee97fe4e41a5fd78afae534999286a
rubencnik <rubehbardakhchyan@gmail.com>
2025-09-09 12:53:46 +0300

    Add files via upload

---
+++ phython.py
@@ -0,0 +1,11 @@
+#Создание массива данных типа "список" на python
+
+fruits = ["apple", "banana", "orange", "kivi"]
+
+#Организация стека на python
+
+stack = []
+
+stack.append("a")
+stack.append("b")
+stack.append("c")
