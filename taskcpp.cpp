b1217691fa6b516ee7be38ffd0c005bd8fcc8aae
rubencnik <rubehbardakhchyan@gmail.com>
2025-09-13 15:18:35 +0300

    Add files via upload

---
+++ taskcpp.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+
+#Создание списка на языке C++
+
+list <string> li {"'a','1','b','2','c','3'"};
+
+#Создание stack на языке C++
+
+stack <string> stack;
+stack.push("a");
+stack.push("b");
+stack.push("c");
