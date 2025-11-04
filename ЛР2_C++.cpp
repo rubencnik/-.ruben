73d4a9c451b6a36907e0900d01e1103ebf9259ec
rubencnik <rubehbardakhchyan@gmail.com>
2025-11-04 16:20:39 +0300

    Add files via upload

---
+++ ЛР2_C++.cpp
@@ -0,0 +1,36 @@
+// Создание мультисписка (вложенного списка):
+struct Node {
+    int data;
+    Node *prev;
+    Node *next;
+};
+
+
+
+
+// Создание очереди:
+std::queue<std::string> queue;
+queue.push("Carrot"); 
+queue.push("Potato"); 
+queue.push("Onion");
+
+
+
+//Создание дека:
+std::deque<int> d = {1, 2, 3};
+
+
+
+//Создание приоритетной очереди с примитивным типом:
+std::priority_queue<int> pq;
+pq.push(10);     
+pq.push(20);    
+pq.push(15); 
+
+
+
+//Создание приоритетной очереди с пользовательскими структурами:
+std::priority_queue<Task> taskQueue;
+taskQueue.push({"Task 1", 2});     
+taskQueue.push({"Task 2", 1});     
+taskQueue.push({"Task 3", 3});  
